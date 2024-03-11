89c2877b7c96c9c811d621051b6958ab8547ec24
notaaash <notaaash@gmail.com>
2024-03-11 18:55:03 +0530

    General Update.

--- app/src/main/java/com/example/raahatapp/CaseFiles.java
+++ app/src/main/java/com/example/raahatapp/CaseFiles.java
@@ -3,12 +3,22 @@ package com.example.raahatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class CaseFiles extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_files);
+
+        webView = findViewById(R.id.courtfilewebview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://services.ecourts.gov.in/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
